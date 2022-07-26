ed0ea0b42f65cd30bfb08d1c9f429ced1b884ff8
AllezitoBR <alle_andrade123@hotmail.com>
2022-07-26 14:56:27 -0300

    Conflitos

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+<<<<<<< HEAD
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var loginRouter = require("./routes/login");
 var productRouter = require("./routes/produto");
 var descriptionRouter = require("./routes/descriptionRoute");
+=======
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+<<<<<<< HEAD
+var checkoutRouter = require('./routes/checkout');
+=======
+>>>>>>> 139d6124c3422e4649c1fa627d49691c91bcc872
+>>>>>>> master
 
 var app = express();
 
@@ -25,12 +34,18 @@ app.use(express.static(path.join(__dirname, "public/images")));
 app.use(express.static(path.join(__dirname, "public/stylesheets")));
 app.use(express.static(path.join(__dirname, "views/partials")));
 
+<<<<<<< HEAD
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 app.use("/pratoFundo", productRouter);
 app.use("/pratoRaso", productRouter);
 app.use("/description", descriptionRouter);
+=======
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/', checkoutRouter);
+>>>>>>> master
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
