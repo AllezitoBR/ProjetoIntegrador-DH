062bda7c35ea7d93da5bc5fba814e4720284892d
educavalcante <eduardo@cpcbrasil.com>
2022-08-03 08:57:36 -0300

    checkout responsivo 1 e 2

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var checkoutRouter = require('./routes/checkout');
 
 var app = express();
+const session = require('express-session');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'frase secreta projeto integrador',
+  resave: true,
+  saveUninitialized: true
+}));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
